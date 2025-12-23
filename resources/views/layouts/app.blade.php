56dd1cc729a3b4a5b3d254a92fcee66f8f476f05
AzizbekJumanazarov <fais_le@mail.ru>
2025-12-23 17:13:15 +0500

    Adding other settings for SEO

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <title>Figma Test by Azizbek Jumanazarov</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Киноафиша, расписание сеансов и билеты">
+    <link rel="icon" href="/favicon.svg">
 </head>
 <body>
 <x-header />
