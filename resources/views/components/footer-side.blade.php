956422d6f260f507e9de293cace33f1cf0506740
AzizbekJumanazarov <fais_le@mail.ru>
2025-12-23 16:58:13 +0500

    Making separate components for footer and header helpers

---
+++ resources/views/components/footer-side.blade.php
@@ -0,0 +1,19 @@
+<div class="header-inner">
+    <div>
+        <a href="/" class="logo">
+            <img src="{{ asset('images/icons/komek-icon.svg') }}" alt="Komek" />
+        </a>
+    </div>
+    <div class="header-address">
+        <p>admin@example.com<br>ticket@example.com</p>
+        <p>улица Тестовая 1</p>
+    </div>
+    <div class="header-phone">
+        <p>+7 (775) 000 00 01 <span><img src="{{ asset('images/icons/dropdown.svg') }}" alt="phone" /></span></p>
+        <p>Касса</p>
+    </div>
+    <div class="header-address">
+        <p>08:00 - 22:00 с пн.-пт.</p>
+        <p>График работы</p>
+    </div>
+</div>
---
+++ resources/views/components/header-side.blade.php
@@ -0,0 +1,30 @@
+<div class="header-inner">
+    <a href="/">
+        <img src="{{ asset('images/icons/komek-icon.svg') }}" alt="Komek" />
+    </a>
+    <div class="header-inner-small">
+        <div class="header-address">
+            <p>Казахстан</p>
+            <p>улица Тестовая 1</p>
+        </div>
+        <div class="header-phone">
+            <p>+7 (775) 000 00 01 <span><img src="{{ asset('images/icons/dropdown.svg') }}" alt="phone" /></span></p>
+            <p>Касса</p>
+        </div>
+    </div>
+    <div class="social-section">
+        <a href="#">
+            <img src="{{ asset('images/icons/facebook.svg') }}" alt="Facebook">
+        </a>
+        <a href="#">
+            <img src="{{ asset('images/icons/instagram.svg') }}" alt="Instagram">
+        </a>
+        <a href="#">
+            <img src="{{ asset('images/icons/youtube.svg') }}" alt="YouTube">
+        </a>
+        <a href="#">
+            <img src="{{ asset('images/icons/vk.svg') }}" alt="VK">
+        </a>
+        <button class="button main-button">Мне пришёл билет</button>
+    </div>
+</div>
